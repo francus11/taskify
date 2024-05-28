99f662a46a8b3480c4779c9e71fdaf94f86a198f
michalBak <lenovo.bak2002@gmail.com>
2024-05-28 12:43:06 +0200

    ...

---
+++ taskify.client/src/views/KanbanPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Layout from './Layout';
+import './styles/global.css';
+import './styles/home.css';
+import './styles/kanbanPage.css';
+
+import TopBar from '../components/topBar';
+import Menu from '../components/sidebarMenu';
+import KanbanBoard from '../components/KanbanBoard';
+
+
+const KanbanPage = () => {
+  return (
+    <Layout>
+      <TopBar />
+      <main>
+        <Menu />
+        <div className='Kanban'>
+          <div className='Kanban__header'>
+            <h2 className='header'>Kanban</h2>
+          </div>
+          <KanbanBoard />
+        </div>
+      </main>
+    </Layout>
+
+ 
+
+  );
+}
+
+export default KanbanPage;
