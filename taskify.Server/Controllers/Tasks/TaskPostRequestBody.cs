e3cea73eb9faeb9d294c8b4fa681be553c7c99c9
francus11 <francus62@gmail.com>
2024-05-31 00:18:07 +0200

    Added required annotations

--- taskify.Server/Controllers/Tasks/TaskPostRequestBody.cs
+++ taskify.Server/Controllers/Tasks/TaskPostRequestBody.cs
@@ -1,14 +1,22 @@
-﻿namespace taskify.Server.Controllers.Tasks
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace taskify.Server.Controllers.Tasks
 {
     public class TaskPostRequestBody
     {
+        [Required]
         public int AuthorId {  get; set; }
+        [Required]
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime? DueTo { get; set; }
+        [Required]
         public int Priority {  get; set; }
+        [Required]
         public int Difficulty { get; set; }
         public string TagName { get; set; }
+        [Required]
         public int ProjectId { get; set; }
     }
 }
