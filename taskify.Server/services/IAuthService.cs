de1cddac787b78009db475f09dab5dccf7bbba3b
francus11 <francus62@gmail.com>
2024-06-02 17:19:20 +0200

    Implemented jwt with refresh token

---
+++ taskify.Server/services/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using NuGet.Protocol.Plugins;
+using taskify.Server.Controllers.Auth;
+using taskify.Server.Models;
+
+namespace taskify.Server.Services
+{
+    public interface IAuthService
+    {
+        public Task<LoginResponse> Login(LoginUser user);
+        public Task<LoginResponse> RefreshToken(RefreshTokenModel model);
+        public Task<bool> RegisterUser(RegisterRequest request);
+    }
+}
